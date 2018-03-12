97ed60be8e237091d077bedacfcaaf6a496b6c3d
namaki <1521939@iub.edu.bd>
2018-03-12 03:32:40 +0000

    assignment10

--- routes/article.server.routes.js
+++ routes/article.server.routes.js
@@ -14,6 +14,9 @@ module.exports = function(app){
 	app.route('/api/articles/edit/:articleId')
 	.get(articles.read)
 	.put(users.requiresLogin, articles.update);
+	
+	app.route('/articles/all').get(articles.listView);
+		app.route('/articles/:articleId').get(articles.singleView);
 
 
 app.param('articleId', articles.articleByID);
